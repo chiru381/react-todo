039a2766ca51e33fe9d655a3b7860c95c3a55149
chiru381 <chirukosanam123@gmail.com>
2022-07-01 18:45:35 +0530

    react todo

---
+++ src/one.js
@@ -0,0 +1,7 @@
+export interface DefaultState {
+    toastMessage: (message: string, severity?: AlertProps['severity'], duration?: number)
+}
+
+export interface DefaultState {
+    toastMessage: (message: String, severity?: AlertProps['severity'], duration?: number)
+}
